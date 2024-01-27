0d879e675ff11064f58482f8ebc5d6682eeb1902
görkem serin <gorkemserin@outlook.com>
2024-01-27 15:46:10 +0300

    Dark Theme on Local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,27 @@ export default function App() {
   const [query, setQuery] = useState("");
   // -------------------------------- //
     /* Dark Mode */
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [theme, setTheme] = useState(() => {
+    const initialTheme = localStorage.getItem("theme");
+    return initialTheme ? initialTheme : "light"
+  });
 
-  function toggleDarkMode (){
-    setIsDarkMode(!isDarkMode);
+  function getThemeFromLocalStorage() {
+    const savedTheme = localStorage.getItem("theme");;
+    if (savedTheme) {
+      setTheme(savedTheme)
+    }
   }
+  function toggleDarkMode (){
+    setTheme((prevTheme) => {
+      const newTheme = prevTheme === "light" ? "dark" : "light";
+      localStorage.setItem("theme",newTheme);
+      return newTheme;
+    })
+  }
+  useEffect(() => {
+    getThemeFromLocalStorage();
+  },[theme])
   // -------------------------------- //
   /* Note filter */
   function filterItems(notes, query) {
@@ -82,16 +98,15 @@ export default function App() {
     const newNotes = notes.filter((note) => note.id !== noteId);
     setNotes(newNotes);
   }
-  // -------------------------------- //
 
-  // -------------------------------- //
   /* Save notes to local storage */
   useEffect(() => {
     localStorage.setItem("notes", JSON.stringify(notes));
   }, [notes]);
+  
   // -------------------------------- //
   return (
-    <div className={`${isDarkMode ? "dark bg-dark" : "bg-light"} `}>
+    <div className={`${theme === "dark" ? "dark bg-dark" : "bg-light"} `}>
       <main className="max-container min-h-screen p-4 dark:text-text-dark text-text-light">
         <section className="flex w-full items-center justify-between py-2">
           <Header toggleDarkMode={toggleDarkMode} />
